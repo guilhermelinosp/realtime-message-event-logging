f10c5ee90f051502e1a533d25ad2386e60d562ee
Guilherme Lino <guilhermelinosp@gmail.com>
2023-08-30 07:32:46 -0300

    feat: add WatchDog Middleware logging

--- Sample.API/Program.cs
+++ Sample.API/Program.cs
@@ -1,15 +1,22 @@
+using WatchDog;
+using WatchDog.src.Enums;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddWatchDogServices(opt =>
+{
+    opt.IsAutoClear = false;
+    //opt.ClearTimeSchedule = WatchDogAutoClearScheduleEnum.Weekly;
+    opt.SetExternalDbConnString = builder.Configuration.GetConnectionString("PostgreSQL");
+    opt.DbDriverOption = WatchDogDbDriverEnum.PostgreSql;
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -17,9 +24,12 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
+app.UseWatchDogExceptionLogger();
+app.UseWatchDog(opt =>
+{
+    opt.WatchPageUsername = "admin";
+    opt.WatchPagePassword = "12345678";
+});
 app.Run();
